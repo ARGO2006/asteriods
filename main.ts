f47f958a6c47c0693fb9768a3639308068722e6e
ARGO2006 <78601744+ARGO2006@users.noreply.github.com>
2021-04-21 08:45:49 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,52 @@ namespace SpriteKind {
     export const A1 = SpriteKind.create()
     export const A2 = SpriteKind.create()
 }
+function mini_asteriods2 () {
+    h = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 1 . . . . . . . . . 
+        . . . . . 1 1 1 . . . . . . . . 
+        . . . . . 1 f 1 1 1 . . . . . . 
+        . . . . 1 1 f f f 1 . . . . . . 
+        . . . . 1 f f f 1 1 . . . . . . 
+        . . . 1 1 f f 1 1 . . . . . . . 
+        . . . 1 1 f 1 1 . . . . . . . . 
+        . . . . 1 1 1 . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.A1)
+    p = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 1 . . . . . . . . . 
+        . . . . . 1 1 1 . . . . . . . . 
+        . . . . . 1 f 1 1 1 . . . . . . 
+        . . . . 1 1 f f f 1 . . . . . . 
+        . . . . 1 f f f 1 1 . . . . . . 
+        . . . 1 1 f f 1 1 . . . . . . . 
+        . . . 1 1 f 1 1 . . . . . . . . 
+        . . . . 1 1 1 . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.A1)
+    mySprite3.setVelocity(55, randint(-5, 5))
+    o.setVelocity(55, randint(-5, 5))
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.A2, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    info.changeScoreBy(10)
+    info.changeLifeBy(-1)
+    mySprite.setPosition(randint(15, 50), randint(15, 50))
+})
 function Call_Asteriods () {
     mySprite2 = sprites.create(img`
         . . . . . . . . . . . . . . . . 
@@ -39,8 +85,8 @@ function Call_Asteriods () {
         . . . . 1 1 f f f f 1 . . . . . 
         . . . . . 1 1 1 1 1 . . . . . . 
         `, SpriteKind.Enemy)
-    mySprite2.setPosition(randint(5, 150), 0)
-    sp3.setPosition(randint(5, 150), 0)
+    mySprite2.setPosition(randint(5, 150), 5)
+    sp3.setPosition(randint(5, 150), 7)
     mySprite2.setVelocity(0, randint(15, 50))
     sp3.setVelocity(0, randint(15, 50))
 }
@@ -63,6 +109,97 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         `, mySprite, 0, -50)
+})
+function Call_Asteriods2 () {
+    k = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . 1 1 1 1 1 1 1 . . . . 
+        . . . . . . 1 f f f f f 1 . . . 
+        . . . . . . 1 f f f f f f 1 . . 
+        . . . . . . 1 f f f f f f f 1 . 
+        . . . . . 1 1 f f f f f f f 1 . 
+        . . 1 1 1 1 f f f f f f f f 1 . 
+        . . 1 f f f f f f f f f f f 1 . 
+        . . 1 f f f f f f f f f f f 1 . 
+        . . 1 f f f f f f f f f f f 1 . 
+        . . 1 f f f f f f f f f f f 1 . 
+        . 1 f f f f f f f f f f f 1 . . 
+        . 1 f f f f f f f f f f 1 . . . 
+        . 1 f f f 1 1 1 1 1 1 1 . . . . 
+        . 1 f f 1 1 1 . . . . . . . . . 
+        . 1 1 1 1 . . . . . . . . . . . 
+        `, SpriteKind.A2)
+    l = sprites.create(img`
+        . . . . 1 1 1 1 1 1 1 . . . . . 
+        . . . . 1 f f f f f 1 1 . . . . 
+        . . . 1 1 f f f f f f 1 . . . . 
+        1 1 1 1 f f f f f f f 1 . . . . 
+        1 f f f f f f f f f f 1 . . . . 
+        1 f f f f f f f f f f 1 1 1 1 1 
+        1 f f f f f f f f f f f f f f 1 
+        1 f f f f f f f f f f f f f f 1 
+        1 1 f f f f f f f f f f f f f 1 
+        . 1 f f f f f f f f f f f f 1 1 
+        . 1 1 f f f f f f f f f 1 1 1 . 
+        . . 1 1 f f f f f f f 1 1 . . . 
+        . . . 1 f f f f f f 1 1 . . . . 
+        . . . . 1 f f f f f 1 . . . . . 
+        . . . . 1 1 f f f f 1 . . . . . 
+        . . . . . 1 1 1 1 1 . . . . . . 
+        `, SpriteKind.A2)
+    k.setPosition(0, randint(5, 150))
+    l.setPosition(0, randint(5, 150))
+    k.setVelocity(randint(15, 50), randint(1, 10))
+    l.setVelocity(randint(15, 50), randint(1, 10))
+}
+function Call_Asteriods3 () {
+    yo = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . 1 1 1 1 1 1 1 . . . . 
+        . . . . . . 1 f f f f f 1 . . . 
+        . . . . . . 1 f f f f f f 1 . . 
+        . . . . . . 1 f f f f f f f 1 . 
+        . . . . . 1 1 f f f f f f f 1 . 
+        . . 1 1 1 1 f f f f f f f f 1 . 
+        . . 1 f f f f f f f f f f f 1 . 
+        . . 1 f f f f f f f f f f f 1 . 
+        . . 1 f f f f f f f f f f f 1 . 
+        . . 1 f f f f f f f f f f f 1 . 
+        . 1 f f f f f f f f f f f 1 . . 
+        . 1 f f f f f f f f f f 1 . . . 
+        . 1 f f f 1 1 1 1 1 1 1 . . . . 
+        . 1 f f 1 1 1 . . . . . . . . . 
+        . 1 1 1 1 . . . . . . . . . . . 
+        `, SpriteKind.A2)
+    co = sprites.create(img`
+        . . . . 1 1 1 1 1 1 1 . . . . . 
+        . . . . 1 f f f f f 1 1 . . . . 
+        . . . 1 1 f f f f f f 1 . . . . 
+        1 1 1 1 f f f f f f f 1 . . . . 
+        1 f f f f f f f f f f 1 . . . . 
+        1 f f f f f f f f f f 1 1 1 1 1 
+        1 f f f f f f f f f f f f f f 1 
+        1 f f f f f f f f f f f f f f 1 
+        1 1 f f f f f f f f f f f f f 1 
+        . 1 f f f f f f f f f f f f 1 1 
+        . 1 1 f f f f f f f f f 1 1 1 . 
+        . . 1 1 f f f f f f f 1 1 . . . 
+        . . . 1 f f f f f f 1 1 . . . . 
+        . . . . 1 f f f f f 1 . . . . . 
+        . . . . 1 1 f f f f 1 . . . . . 
+        . . . . . 1 1 1 1 1 . . . . . . 
+        `, SpriteKind.A2)
+    yo.setPosition(150, randint(5, 150))
+    co.setPosition(150, randint(5, 150))
+    yo.setVelocity(randint(15, 50), randint(1, 10))
+    co.setVelocity(randint(15, 50), randint(1, 10))
+}
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.A2, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    info.changeScoreBy(100)
+    mini_asteriods2()
+    h.setPosition(otherSprite.x, otherSprite.y)
+    p.setPosition(otherSprite.x, otherSprite.y)
 })
 function mini_asteriods () {
     mySprite3 = sprites.create(img`
@@ -104,6 +241,11 @@ function mini_asteriods () {
     mySprite3.setVelocity(randint(-5, 5), 55)
     o.setVelocity(randint(-5, 5), 55)
 }
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.A1, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    sprite.destroy()
+    info.changeScoreBy(50)
+})
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
     otherSprite.destroy()
     info.changeScoreBy(100)
@@ -114,13 +256,20 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, oth
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     otherSprite.destroy()
     info.changeLifeBy(-1)
+    info.changeScoreBy(10)
     mySprite.setPosition(randint(15, 50), randint(15, 50))
 })
-let o: Sprite = null
-let mySprite3: Sprite = null
+let co: Sprite = null
+let yo: Sprite = null
+let l: Sprite = null
+let k: Sprite = null
 let projectile: Sprite = null
 let sp3: Sprite = null
 let mySprite2: Sprite = null
+let o: Sprite = null
+let mySprite3: Sprite = null
+let p: Sprite = null
+let h: Sprite = null
 let mySprite: Sprite = null
 scene.setBackgroundImage(img`
     ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
@@ -264,12 +413,15 @@ mySprite = sprites.create(img`
 controller.moveSprite(mySprite, 100, 100)
 info.setLife(3)
 game.onUpdateInterval(5000, function () {
-    if (info.score() > 1000) {
+    if (info.score() > 500) {
         for (let index = 0; index < 4; index++) {
             Call_Asteriods()
+            Call_Asteriods2()
         }
         Call_Asteriods()
+        Call_Asteriods2()
     } else {
         Call_Asteriods()
+        Call_Asteriods2()
     }
 })
